7921bbdd1c7425b5c0e4c7b626a2af47e4fdda8e
Prastiko Sasmitho <prastiko46@gmail.com>
2020-11-06 16:46:39 +0700

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -26,15 +26,15 @@ async def amireallyalive(alive):
         reply_to_id = await alive.get_reply_message()
     if CAT_IMG:
         cat_caption = f"<b>{JISAN}</b>\n\n"
-        cat_caption += f"<b> »» [ 👤 ] ➥ My Master : {hmention}</b>\n"
-        cat_caption += f"<b> »» [ 🕒 ] ➥ Uptime :</b> <code>{uptime}</code>\n"
-        cat_caption += f"<b> »» [ 🐍 ] ➥ Python :</b> <code>{python_version()}</code>\n"
+        cat_caption += f"<b> ✘   [   👤   ]  ➥  My Master : {hmention}</b>\n"
+        cat_caption += f"<b> ✘  [   🕒   ]  ➥  Uptime :</b> <code>{uptime}</code>\n"
+        cat_caption += f"<b> ✘   [   🐍   ]  ➥  Python :</b> <code>{python_version()}</code>\n"
         cat_caption += (
-            f"<b> »» [ ⚙️ ] ➥ Telethon :</b> <code>{version.__version__}</code>\n"
+            f"<b>✘   [   ⚙️   ]  ➥ Telethon :</b> <code>{version.__version__}</code>\n"
         )
-        cat_caption += f"<b> »» [ 🤖 ] ➥ Bot :</b> <code>{catversion}</code>\n"
-        cat_caption += f"<b> »» [ 💻 ] ➥ Database :</b> <code>{check_sgnirts}</code>\n\n"
-        cat_caption += "   | <a href = https://github.com/Akarata><b>Click here if you want to know about Me</b></a> | "
+        cat_caption += f"<b> ✘   [   🤖   ]  ➥  Project_Akarata :</b>1.0\n"
+        cat_caption += f"<b> ✘   [   💻   ]  ➥  Database :</b> <code>{check_sgnirts}</code>\n\n"
+        cat_caption += "   ✘   <a href = https://github.com/Akarata><b>Click here if you want to know about Me</b></a>   ✘ "
 
         await alive.client.send_file(
             alive.chat_id,
