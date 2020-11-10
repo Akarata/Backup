7a5891a39939bdfc95e0359226a3a5856e396002
Akarata <70458149+Akarata@users.noreply.github.com>
2020-11-10 05:16:16 +0000

    pylint: auto fixes

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -44,9 +44,7 @@ for name in files:
 
 LOGS.info("Yay your userbot is officially working.!!!")
 LOGS.info("Congratulation, now type .alive to see message if bot is live\n")
-LOGS.info(
-    "Repo based on catuserbot and Modified by Akarata\n"
-)
+LOGS.info("Repo based on catuserbot and Modified by Akarata\n")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
