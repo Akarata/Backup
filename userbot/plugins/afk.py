1be520de80a944ff65c3cc73d47a66ab47a5b766
Prastiko Sasmitho <prastiko46@gmail.com>
2020-11-16 13:23:37 +0700

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -107,7 +107,7 @@ async def on_afk(event):
     if USERAFK_ON and not (await event.get_sender()).bot:
         msg = None
         message_to_reply = (
-            f"**Aku Sedang Offline**\n\n**Offline Sejak :** `{endtime}` **yang lalu** \n**Karena : **{reason}\n<b>Mungkin Aku akan kembali dalam beberapa tahun :3</b>"
+            f"**Aku Sedang Offline**\n\n**Offline Sejak :** `{endtime}` **yang lalu**" + f"\n\n__Mungkin Aku akan kembali dalam beberapa tahun :3__\n**Karena Aku**: {reason}"
             if reason
             else f"**Aku Sedang Offline**\n\n**Offline Sejak :** `{endtime}` **yang lalu** \n**Karena : **`Busy AF ( if important u can PM )`"
         )
